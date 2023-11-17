487786ba936fbb7fce06163c25ed64f2c321634f
shubham bramhane <shubham.bramhane76@gmail.com>
2023-11-18 01:01:45 +0530

    roles

--- resources/views/admin/layout/partial/alert.blade.php
+++ resources/views/admin/layout/partial/alert.blade.php
@@ -2,7 +2,7 @@
 @if (Session::has('success'))
     <div class="alert alert-success alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fa-times"></i>
+             <span aria-hidden="true">&times;</span>
         </button>
         <strong>Success !</strong> {{ session('success') }}
     </div>
@@ -11,7 +11,7 @@
 @if (Session::has('error'))
     <div class="alert alert-danger alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fa-times"></i>
+             <span aria-hidden="true">&times;</span>
         </button>
         <strong>Error !</strong> {{ session('error') }}
     </div>
@@ -20,7 +20,7 @@
 @if (Session::has('warning'))
     <div class="alert alert-warning alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fa-times"></i>
+             <span aria-hidden="true">&times;</span>
         </button>
         <strong>Warning !</strong> {{ session('warning') }}
     </div>
@@ -29,7 +29,7 @@
 @if (Session::has('info'))
     <div class="alert alert-info alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fa-times"></i>
+             <span aria-hidden="true">&times;</span>
         </button>
         <strong>Info !</strong> {{ session('info') }}
     </div>
@@ -41,7 +41,7 @@
 @if ($errors->any())
     <div class="alert alert-danger alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fa-times"></i>
+             <span aria-hidden="true">&times;</span>
         </button>
         <strong>Error !</strong>
         <ul>
