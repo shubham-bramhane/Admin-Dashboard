a95ee0ebbb861d0370ed553f6dea01d5db7d87c3
shubham bramhane <shubham.bramhane76@gmail.com>
2023-11-29 21:52:47 +0530

    add _construct in user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -12,6 +12,13 @@ use Validator;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('permission:users-list|users-create|users-edit|users-delete', ['only' => ['index', 'store']]);
+        $this->middleware('permission:users-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:users-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:users-delete', ['only' => ['destroy']]);
+    }
     /**
      * Display a listing of the resource.
      */
